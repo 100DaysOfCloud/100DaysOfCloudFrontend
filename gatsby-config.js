1be9c39bf314e0d9a1a45433d9771bb9559987ee
Antonio Lo Fiego <al3359@drexel.edu>
2020-07-17 14:15:24 -0400

    Initial Commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,24 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: `100DaysOfCloud`,
+    description: `Official Website of the #100DaysOfCloud Community Challenge`,
+    author: `@antonio_lofiego`,
+  },
+  plugins: [
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-postcss`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `100DaysOfCloud`,
+        short_name: `100DOC`,
+        start_url: `/`,
+        icon: `static/favicon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
 }
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    purge: [],
+    theme: {
+        extend: {
+            maxWidth: {
+                '7xl': '80rem',
+                '8xl': '88rem',
+            },
+        },
+    },
+    variants: {},
+    plugins: [`gatsby-plugin-postcss`],
+};

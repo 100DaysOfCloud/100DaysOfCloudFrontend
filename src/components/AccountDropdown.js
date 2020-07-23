a488bd2346297bbe9b079a921e72932262195551
Antonio Lo Fiego <alofiego@hey.com>
2020-07-23 01:56:23 -0400

    Fix background color error for dropdown

--- src/components/AccountDropdown.js
+++ src/components/AccountDropdown.js
@@ -39,17 +39,18 @@ class AccountDropdown extends Component {
 
     render() {
         return (
-            <div className='relative'>
+            <div className="relative">
                 <button
                     className={
                         'relative z-10 block h-16 w-16 rounded-full overflow-hidden border-2 focus:outline-none' +
                         (this.state.clicked ? ' focus:border-gray-500' : '')
                     }
-                    onClick={this.handleClick}>
+                    onClick={this.handleClick}
+                >
                     <img
-                        className='h-full w-full object-cover object-center'
-                        src='https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80'
-                        alt='Your profile'
+                        className="h-full w-full object-cover object-center"
+                        src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80"
+                        alt="Your profile"
                     />
                 </button>
                 <button
@@ -59,26 +60,30 @@ class AccountDropdown extends Component {
                             ? 'fixed inset-0 h-full w-full cursor-default focus:outline-0'
                             : 'hidden '
                     }
-                    tabindex='-1'
+                    tabindex="-1"
                 />
                 <div
                     className={
                         (this.state.clicked ? 'block ' : 'hidden ') +
-                        'bg-grey-200 w-48 py-2 mt-4 rounded-lg shadow-xl absolute right-0'
-                    }>
+                        'bg-gray-200 w-48 py-2 mt-4 rounded-lg shadow-xl absolute right-0'
+                    }
+                >
                     <Link
-                        href='/settings'
-                        className='block px-4 py-2 text-gray-800 hover:bg-gray-400'>
+                        href="/settings"
+                        className="block px-4 py-2 text-gray-800 hover:bg-gray-400"
+                    >
                         Account setting
                     </Link>
                     <Link
-                        href='/submit'
-                        className='block px-4 py-2 text-gray-800 hover:bg-gray-400'>
+                        href="/submit"
+                        className="block px-4 py-2 text-gray-800 hover:bg-gray-400"
+                    >
                         Submit an article
                     </Link>
                     <Link
-                        href='/log-out'
-                        className='block px-4 py-2 text-gray-800 hover:bg-gray-400'>
+                        href="/log-out"
+                        className="block px-4 py-2 text-gray-800 hover:bg-gray-400"
+                    >
                         Log out
                     </Link>
                 </div>

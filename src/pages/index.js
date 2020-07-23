f19a0f705100b88009a20515b3ac6116abdff879
Antonio Lo Fiego <alofiego@hey.com>
2020-07-23 01:58:41 -0400

    Organize homepage with CSS Grid

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,30 @@
-import React from "react"
+import React from 'react';
 
-import Header from "../components/Header"
+import Header from '../components/Header';
+import FeaturedArticle from '../components/FeaturedArticle';
+import ArticleThumbnail from '../components/ArticleThumbnail';
+import NewsCarousel from '../components/NewsCarousel';
 
-import "../styles.css"
+import '../styles.css';
 
 export default function Home() {
-  return (
-    <div>
-      <Header />
-    </div>
-  )
+    return (
+        <div className="max-w-8xl m-auto">
+            <Header />
+            <div className="grid grid-cols-10 gap-8">
+                <div className="col-span-2">
+                    <ArticleThumbnail provider="medium" />
+                </div>
+                <div className="col-span-5">
+                    <FeaturedArticle />
+                    <NewsCarousel category="AWS" />
+                    <NewsCarousel category="Azure" />
+                    <NewsCarousel category="Google Cloud" />
+                </div>
+                <div className="col-span-3">
+                    <ArticleThumbnail provider="dev" />
+                </div>
+            </div>
+        </div>
+    );
 }

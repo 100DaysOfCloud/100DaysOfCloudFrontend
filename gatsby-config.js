6e58f6e73ce14466aef5da85fc60bf4d80022465
Antonio Lo Fiego <alofiego@hey.com>
2020-07-23 01:55:24 -0400

    Move from Yarn to npm

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,24 +1,24 @@
 module.exports = {
-  siteMetadata: {
-    title: `100DaysOfCloud`,
-    description: `Official Website of the #100DaysOfCloud Community Challenge`,
-    author: `@antonio_lofiego`,
-  },
-  plugins: [
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    `gatsby-plugin-postcss`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `100DaysOfCloud`,
-        short_name: `100DOC`,
-        start_url: `/`,
-        icon: `static/favicon.png`, // This path is relative to the root of the site.
-      },
+    siteMetadata: {
+        title: `100DaysOfCloud`,
+        description: `Official Website of the #100DaysOfCloud Community Challenge`,
+        author: `@antonio_lofiego`,
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
-}
+    plugins: [
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        `gatsby-plugin-postcss`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `100DaysOfCloud`,
+                short_name: `100DOC`,
+                start_url: `/`,
+                icon: `static/favicon.ico`, // This path is relative to the root of the site.
+            },
+        },
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.dev/offline
+        // `gatsby-plugin-offline`,
+    ],
+};

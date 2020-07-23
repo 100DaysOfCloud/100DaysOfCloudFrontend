edf1adade71a63aef3cdae9c1f60ebf4993a9166
Antonio Lo Fiego <alofiego@hey.com>
2020-07-23 01:57:29 -0400

    Create basic article thumbnail component

---
+++ src/components/ArticleThumbnail.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+import { FaMedium, FaDev, FaLinkedin } from 'react-icons/fa';
+
+export default class ArticleThumbnail extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    components = {
+        medium: FaMedium,
+        dev: FaDev,
+        linkedin: FaLinkedin,
+    };
+
+    render() {
+        const Provider = this.components[this.props.provider];
+        return (
+            <div>
+                <div>
+                    <div className="absolute ml-2 mt-2 bg-gray-100 rounded-lg">
+                        {Provider && <Provider size={64} />}
+                    </div>
+                    <img src="https://images.unsplash.com/photo-1528642474498-1af0c17fd8c3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" />
+                </div>
+                <div>
+                    <p className="text-2xl text-bold">An article title</p>
+                    <p className="text-xl">An article subtitle</p>
+                </div>
+            </div>
+        );
+    }
+}

bc1abcda7549a472fd1a4a3cfc29947981e26f1d
Bruck1701 <kummel@gmail.com>
2020-07-24 11:04:23 +0200

    refactor: use react-icons on footer and refactor style.css

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Header from '../components/Header';
 import FeaturedArticle from '../components/FeaturedArticle';
 import ArticleThumbnail from '../components/ArticleThumbnail';
 import NewsCarousel from '../components/NewsCarousel';
+import Footer from '../components/Footer';
 
 import '../styles.css';
 
@@ -11,19 +12,25 @@ export default function Home() {
     return (
         <div className="max-w-8xl m-auto">
             <Header />
-            <div className="grid grid-cols-10 gap-8">
-                <div className="col-span-2">
-                    <ArticleThumbnail provider="medium" />
+
+            <div className="site">
+                <div className="site-content">
+                    <div className="grid grid-cols-10 gap-8">
+                        <div className="col-span-2">
+                            <ArticleThumbnail provider="medium" />
+                        </div>
+                        <div className="col-span-5">
+                            <FeaturedArticle />
+                            <NewsCarousel category="AWS" />
+                            <NewsCarousel category="Azure" />
+                            <NewsCarousel category="Google Cloud" />
+                        </div>
+                        <div className="col-span-3">
+                            <ArticleThumbnail provider="dev" />
+                        </div>
+                    </div>
                 </div>
-                <div className="col-span-5">
-                    <FeaturedArticle />
-                    <NewsCarousel category="AWS" />
-                    <NewsCarousel category="Azure" />
-                    <NewsCarousel category="Google Cloud" />
-                </div>
-                <div className="col-span-3">
-                    <ArticleThumbnail provider="dev" />
-                </div>
+                <Footer />
             </div>
         </div>
     );

e8b81e669acf6391195c0b35d6fedd6de298f3c8
Antonio Lo Fiego <alofiego@hey.com>
2020-07-30 15:44:31 -0400

    Lint code

--- src/templates/rules.js
+++ src/templates/rules.js
@@ -1,38 +1,39 @@
-import React from "react"
-import { graphql } from "gatsby"
+import React from 'react';
+import { graphql } from 'gatsby';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
-import '../rules.css'
-
+import '../rules.css';
 
 export default function Template({
-  data, // this prop will be injected by the GraphQL query below.
+    data, // this prop will be injected by the GraphQL query below.
 }) {
-  const { markdownRemark } = data // data.markdownRemark holds your post data
-  const { html } = markdownRemark
-  console.log(html)
-  
-  
-  return (
-    <div className="max-w-8xl m-auto">
-    <Header />
+    const { markdownRemark } = data; // data.markdownRemark holds your post data
+    const { html } = markdownRemark;
+    console.log(html);
 
-      <div className="site">
-          <div className="site-content">
-            <br />
-            <div className="rules" dangerouslySetInnerHTML={{ __html: html}}  />
-          </div>
-      <Footer />
-      </div>
-      </div>
-  );
+    return (
+        <div className="max-w-8xl m-auto">
+            <Header />
+
+            <div className="site">
+                <div className="site-content">
+                    <br />
+                    <div
+                        className="rules"
+                        dangerouslySetInnerHTML={{ __html: html }}
+                    />
+                </div>
+                <Footer />
+            </div>
+        </div>
+    );
 }
 
 export const pageQuery = graphql`
-{
-  markdownRemark {
-    html
-  }
-}
-`
+    {
+        markdownRemark {
+            html
+        }
+    }
+`;

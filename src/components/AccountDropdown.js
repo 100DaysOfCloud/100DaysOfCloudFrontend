b0da1d1652516a4f4648245ee33ab8bf6f259cd8
Antonio Lo Fiego <alofiego@hey.com>
2020-07-30 11:16:44 -0400

    Refactor AccountDropdown as functional component

--- src/components/AccountDropdown.js
+++ src/components/AccountDropdown.js
@@ -1,95 +1,80 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'gatsby';
 
-class AccountDropdown extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            clicked: false,
-        };
+export default function AccountDropdown(props) {
+    const [clicked, setClicked] = useState(false);
+
+    // Handles clicking on the profile image
+    function handleClick(previousClicked) {
+        setClicked(!previousClicked);
     }
 
-    handleClick = () => {
-        this.setState({
-            clicked: !this.state.clicked,
-        });
-    };
-
-    handleClose = () => {
-        this.setState({
-            clicked: false,
-        });
-    };
-
-    escFunction = (event) => {
-        if (event.keyCode === 27) {
-            this.setState({
-                clicked: false,
-            });
-        }
-    };
-
-    componentDidMount() {
-        document.addEventListener('keydown', this.escFunction, false);
+    // Handles clicking anywhere outside of the dropdown menu to close it
+    function handleClose() {
+        setClicked(false);
     }
 
-    componentWillUnmount() {
-        document.removeEventListener('keydown', this.escFunction, false);
+    // Handles clicking ESC to close the dropdown menu
+    function escFunction(event) {
+        if (event.keyCode === 27) setClicked(false);
     }
 
-    render() {
-        return (
-            <div className="relative">
-                <button
-                    className={
-                        'relative z-10 block h-16 w-16 rounded-full overflow-hidden border-2 focus:outline-none' +
-                        (this.state.clicked ? ' focus:border-gray-500' : '')
-                    }
-                    onClick={this.handleClick}
-                >
-                    <img
-                        className="h-full w-full object-cover object-center"
-                        src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80"
-                        alt="Your profile"
-                    />
-                </button>
-                <button
-                    onClick={this.handleClose}
-                    className={
-                        this.state.clicked
-                            ? 'fixed inset-0 h-full w-full cursor-default focus:outline-0'
-                            : 'hidden '
-                    }
-                    tabindex="-1"
+    // On component mount, adds the ESC handler
+    useEffect(() => {
+        document.addEventListener('keydown', escFunction, false);
+
+        return document.removeEventListener('keydown', escFunction, false);
+    });
+
+    return (
+        <div className="relative">
+            <button
+                className={
+                    'relative z-10 block h-16 w-16 rounded-full overflow-hidden border-2 focus:outline-none' +
+                    (clicked ? ' focus:border-gray-500' : '')
+                }
+                onClick={handleClick}
+            >
+                <img
+                    className="h-full w-full object-cover object-center"
+                    src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80"
+                    alt="Your profile"
                 />
-                <div
-                    className={
-                        (this.state.clicked ? 'block ' : 'hidden ') +
-                        'bg-gray-200 w-48 py-2 mt-4 rounded-lg shadow-xl absolute right-0'
-                    }
+            </button>
+            <button
+                onClick={handleClose}
+                className={
+                    clicked
+                        ? 'fixed inset-0 h-full w-full cursor-default focus:outline-0'
+                        : 'hidden '
+                }
+                tabindex="-1"
+            />
+            <div
+                className={
+                    (clicked ? 'block ' : 'hidden ') +
+                    'bg-gray-200 w-48 py-2 mt-4 rounded-lg shadow-xl absolute right-0'
+                }
+            >
+                <Link
+                    href="/settings"
+                    className="block px-4 py-2 text-gray-800 hover:bg-gray-400"
                 >
-                    <Link
-                        href="/settings"
-                        className="block px-4 py-2 text-gray-800 hover:bg-gray-400"
-                    >
-                        Account setting
-                    </Link>
-                    <Link
-                        href="/submit"
-                        className="block px-4 py-2 text-gray-800 hover:bg-gray-400"
-                    >
-                        Submit an article
-                    </Link>
-                    <Link
-                        href="/log-out"
-                        className="block px-4 py-2 text-gray-800 hover:bg-gray-400"
-                    >
-                        Log out
-                    </Link>
-                </div>
+                    Account setting
+                </Link>
+                <Link
+                    href="/submit"
+                    className="block px-4 py-2 text-gray-800 hover:bg-gray-400"
+                >
+                    Submit an article
+                </Link>
+                <Link
+                    href="/log-out"
+                    className="block px-4 py-2 text-gray-800 hover:bg-gray-400"
+                >
+                    Log out
+                </Link>
             </div>
-        );
-    }
+        </div>
+    );
 }
-
-export default AccountDropdown;

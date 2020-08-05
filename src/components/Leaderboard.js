e7f33072bed99cf23f4bfa5d499f9a2120152e36
Antonio Lo Fiego <alofiego@hey.com>
2020-08-04 22:24:22 -0400

    Add score headers in leaderboard

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -7,6 +7,12 @@ import { users } from '../data/users1.json';
 export default function Leaderboard() {
     return (
         <div>
+            <div className="grid grid-cols-12 items-center justify-center relative text-md font-bold">
+                <div className="col-span-9"></div>
+                <p className="col-span-1 mx-auto">GitHub Streak</p>
+                <p className="col-span-1 mx-auto">Twitter Streak</p>
+                <p className="col-span-1 mx-auto">Total Score</p>
+            </div>
             {users.map((user, index) => {
                 return (
                     <LeaderboardEntry

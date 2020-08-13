1409a8ddd16f89dfbb129f0aba700332281eb83b
Antonio Lo Fiego <alofiego@hey.com>
2020-08-13 19:15:00 -0400

    Additions to Tailwind configuration

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    theme: {
+        extend: {
+            // Adds a new breakpoint in addition to the default breakpoints
+            padding: {
+                '16x9': '56.25%',
+            },
+        },
+    },
+};

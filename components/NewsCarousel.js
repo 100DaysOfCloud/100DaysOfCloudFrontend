6f77d9d01db0f253e107434b27413052a7d6ba36
Antonio Lo Fiego <alofiego@hey.com>
2020-08-13 19:15:26 -0400

    Refactor NewsCarousel for Next.js

---
+++ components/NewsCarousel.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import Slider from 'react-slick';
+
+import ArticleThumbnail from './ArticleThumbnail';
+
+export default function NewsCarousel(props) {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 3,
+        centerMode: true,
+    };
+
+    return (
+        <div className='pb-8'>
+            <h2 className='text-2xl font-bold border-b-2 border-gray-900 mb-4'>
+                {props.category}
+            </h2>
+            <Slider {...settings}>
+                <ArticleThumbnail provider='medium' />
+                <ArticleThumbnail provider='dev' />
+                <ArticleThumbnail provider='medium' />
+                <ArticleThumbnail provider='medium' />
+                <ArticleThumbnail provider='dev' />
+                <ArticleThumbnail provider='medium' />
+            </Slider>
+        </div>
+    );
+}

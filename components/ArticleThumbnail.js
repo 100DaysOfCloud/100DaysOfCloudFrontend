e95ca3b349b8fd27281f45718aff3c8473b7c761
Antonio Lo Fiego <alofiego@hey.com>
2020-08-13 19:12:48 -0400

    Article-related components in a working state

---
+++ components/ArticleThumbnail.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { FaMedium, FaDev, FaLinkedin } from 'react-icons/fa';
+
+export default function ArticleThumbnail(props) {
+    const components = {
+        medium: FaMedium,
+        dev: FaDev,
+        linkedin: FaLinkedin,
+    };
+
+    const Provider = components[props.provider];
+
+    return (
+        <div>
+            <div>
+                <div className='absolute ml-2 mt-2 bg-gray-100 rounded-lg z-10'>
+                    {Provider && <Provider size={64} />}
+                </div>
+                <div className='w-full pt-16x9 relative overflow-hidden'>
+                    <img
+                        className='h-full w-full object-cover object-center absolute inset-0'
+                        src='https://source.unsplash.com/random/'
+                        alt='Article'
+                    />
+                </div>
+            </div>
+            <div>
+                <p className='text-2xl text-bold'>An article title</p>
+                <p className='text-xl'>An article subtitle</p>
+            </div>
+        </div>
+    );
+}
---
+++ components/FeaturedArticle.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import ArticleThumbnail from './ArticleThumbnail';
+
+export default function FeaturedArticle(props) {
+    return (
+        <div className='pb-8'>
+            <h2 className='text-2xl font-bold border-b-2 border-gray-900 mb-4'>
+                Featured Article
+            </h2>
+            <ArticleThumbnail />
+        </div>
+    );
+}

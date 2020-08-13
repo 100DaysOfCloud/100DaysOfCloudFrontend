c4a9ff1a65dafc39257dd0a613c973ee4c531c9d
Antonio Lo Fiego <alofiego@hey.com>
2020-08-13 19:15:55 -0400

    Refactor homepage for Next.js

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,42 @@
 import Head from 'next/head';
 
+import React from 'react';
+
+import Header from '../components/Header';
+import FeaturedArticle from '../components/FeaturedArticle';
+import ArticleThumbnail from '../components/ArticleThumbnail';
+import NewsCarousel from '../components/NewsCarousel';
+import Footer from '../components/Footer';
+
 export default function Home() {
     return (
-        <div>
+        <div className='w-5/6 m-auto'>
             <Head>
-                <title>100 Days Of Cloud</title>
-                <link rel='icon' href='/favicon.ico' />
+                <title>#100DaysOfCloud</title>
+                <link rel='icon' href='/favicon.png' />
             </Head>
 
-            <main>
-                <h1>Hello</h1>
-            </main>
+            <Header />
+
+            <div className='site'>
+                <div className='site-content'>
+                    <div className='grid grid-cols-10 gap-8'>
+                        <div className='col-span-2'>
+                            <ArticleThumbnail provider='medium' />
+                        </div>
+                        <div className='col-span-5'>
+                            <FeaturedArticle />
+                            <NewsCarousel category='AWS' />
+                            <NewsCarousel category='Azure' />
+                            <NewsCarousel category='Google Cloud' />
+                        </div>
+                        <div className='col-span-3'>
+                            <ArticleThumbnail provider='dev' />
+                        </div>
+                    </div>
+                </div>
+                <Footer />
+            </div>
         </div>
     );
 }

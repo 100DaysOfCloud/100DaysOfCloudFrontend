4e09f907e8d82cefb98f39c725164e15562c157e
Antonio Lo Fiego <alofiego@hey.com>
2020-08-14 02:03:23 -0400

    Modified behavior of tags API endpoint

--- pages/api/articles/tags.js
+++ pages/api/articles/tags.js
@@ -4,7 +4,16 @@ import _ from 'lodash';
 
 export default function returnFilteredArticles(req, res) {
     // Catch all possible routes
-    const tags = _(articles).groupBy('tags').keys();
+    const tags = _(articles)
+        .countBy('tags')
+        .map(function (count, tag) {
+            return {
+                tag: tag,
+                count: count,
+            };
+        })
+        .orderBy('count', 'desc')
+        .slice(0, 20);
 
     res.status(200).json(tags);
 }

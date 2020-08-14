74bb1f4f3f3100ce905bc42a9d5d081d5583f340
Antonio Lo Fiego <alofiego@hey.com>
2020-08-14 00:48:09 -0400

    Featured Article is API dependent

--- components/FeaturedArticle.js
+++ components/FeaturedArticle.js
@@ -2,13 +2,30 @@ import React from 'react';
 
 import ArticleThumbnail from './ArticleThumbnail';
 
+import useSWR from 'swr';
+
+const fetcher = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    if (res.status !== 200) {
+        throw new Error(data.message);
+    }
+    return data;
+};
+
 export default function FeaturedArticle(props) {
+    const { data, error } = useSWR(() => `/api/articles/`, fetcher);
+
+    if (error) return <div>{error.message}</div>;
+    if (!data) return <div>Loading...</div>;
+
+    const { title, subtitle, image } = data[0];
+
     return (
         <div className='pb-8'>
-            <h2 className='text-2xl font-bold border-b-2 border-gray-900 mb-4'>
-                Featured Article
-            </h2>
-            <ArticleThumbnail />
+            <h2 className='section-title'>Featured Article</h2>
+            <ArticleThumbnail title={title} subtitle={subtitle} image={image} />
         </div>
     );
 }

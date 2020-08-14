e1b70c53b6ce3c4c4cbedd6185430aa5fa8c85ff
Antonio Lo Fiego <alofiego@hey.com>
2020-08-14 00:48:29 -0400

    Added API integration to News Carousel

--- components/NewsCarousel.js
+++ components/NewsCarousel.js
@@ -4,6 +4,18 @@ import Slider from 'react-slick';
 
 import ArticleThumbnail from './ArticleThumbnail';
 
+import useSWR from 'swr';
+
+const fetcher = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    if (res.status !== 200) {
+        throw new Error(data.message);
+    }
+    return data;
+};
+
 export default function NewsCarousel(props) {
     const settings = {
         dots: true,
@@ -14,16 +26,28 @@ export default function NewsCarousel(props) {
         centerMode: true,
     };
 
+    const { data, error } = useSWR(
+        () => `/api/articles/${props.category}`,
+        fetcher
+    );
+
+    if (error) return <div>{error.message}</div>;
+    if (!data) return <div>Loading...</div>;
+
+    console.log(data);
+
     return (
         <div className='pb-8'>
             <h2 className='section-title'>{props.category}</h2>
             <Slider {...settings}>
-                <ArticleThumbnail provider='medium' />
-                <ArticleThumbnail provider='dev' />
-                <ArticleThumbnail provider='medium' />
-                <ArticleThumbnail provider='medium' />
-                <ArticleThumbnail provider='dev' />
-                <ArticleThumbnail provider='medium' />
+                {data.map((article, index) => (
+                    <ArticleThumbnail
+                        title={article.title}
+                        subtitle={article.subtitle}
+                        image={article.image}
+                        key={article.id + index}
+                    />
+                ))}
             </Slider>
         </div>
     );

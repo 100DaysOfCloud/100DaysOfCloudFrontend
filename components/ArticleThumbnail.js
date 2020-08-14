4ee66fea02b9e9228c368ca6c503b781386a08df
Antonio Lo Fiego <alofiego@hey.com>
2020-08-13 22:59:23 -0400

    Fix article thumbnail style

--- components/ArticleThumbnail.js
+++ components/ArticleThumbnail.js
@@ -14,7 +14,7 @@ export default function ArticleThumbnail(props) {
     return (
         <div>
             <div>
-                <div className='absolute ml-2 mt-2 bg-gray-100 rounded-lg z-10'>
+                <div className='absolute ml-2 mt-2 bg-gray-100 rounded-lg z-20'>
                     {Provider && <Provider size={64} />}
                 </div>
                 <div className='w-full pt-16x9 relative overflow-hidden'>

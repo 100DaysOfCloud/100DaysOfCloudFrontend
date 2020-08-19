d17d35754ed4a45ad3e1d318c99106c5476f19f8
Antonio Lo Fiego <alofiego@hey.com>
2020-08-19 16:34:20 -0400

    Refactor articles list page to use Layout component

--- pages/articles/[[...tag]].js
+++ pages/articles/[[...tag]].js
@@ -5,6 +5,7 @@ import Head from 'next/head';
 import { useRouter } from 'next/router';
 import useSWR from 'swr';
 import ArticleThumbnail from '../../components/ArticleThumbnail';
+import Layout from '../../components/Layout';
 
 const fetcher = async (url) => {
     const res = await fetch(url);
@@ -36,31 +37,26 @@ export default function User(props) {
     if (!data) return <div>Loading...</div>;
 
     return (
-        <div className='w-5/6 m-auto'>
-            <Head>
-                <title>
-                    {query.tag ? query.tag : 'All'} Articles | #100DaysOfCloud
-                </title>
-                <link rel='icon' href='/favicon.png' />
-            </Head>
+        <div>
+            <Layout title={(query.tag ? query.tag : 'All') + ' Articles'}>
+                <div className='flex justify-between section-title'>
+                    <h2 className=''>
+                        {query.tag ? query.tag : 'All'} articles
+                    </h2>
+                    <span>{data.length} articles available</span>
+                </div>
 
-            <Header />
-
-            <div className='flex justify-between section-title'>
-                <h2 className=''>{query.tag ? query.tag : 'All'} articles</h2>
-                <span>{data.length} articles available</span>
-            </div>
-            <div className='grid grid-cols-3 gap-8'>
-                {data.map((article, index) => (
-                    <ArticleThumbnail
-                        title={article.title}
-                        subtitle={article.subtitle}
-                        image={article.image}
-                        key={article.id + index}
-                    />
-                ))}
-            </div>
-            <Footer />
+                <div className='grid grid-cols-3 gap-8'>
+                    {data.map((article, index) => (
+                        <ArticleThumbnail
+                            title={article.title}
+                            subtitle={article.subtitle}
+                            image={article.image}
+                            key={article.id + index}
+                        />
+                    ))}
+                </div>
+            </Layout>
         </div>
     );
 }

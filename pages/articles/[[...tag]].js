82fe665da84bbf13a84721aff09c244ba9202f83
Antonio Lo Fiego <alofiego@hey.com>
2020-08-19 16:35:42 -0400

    Remove unused imports

--- pages/articles/[[...tag]].js
+++ pages/articles/[[...tag]].js
@@ -1,7 +1,3 @@
-import Header from '../../components/Header';
-import Footer from '../../components/Footer';
-import Head from 'next/head';
-
 import { useRouter } from 'next/router';
 import useSWR from 'swr';
 import ArticleThumbnail from '../../components/ArticleThumbnail';

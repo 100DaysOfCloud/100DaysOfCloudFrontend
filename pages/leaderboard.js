2085593ec713d0e376e7490cd03dfaaa2c81dacc
Antonio Lo Fiego <alofiego@hey.com>
2020-08-19 14:41:16 -0400

    Refactor Leaderboard page to use Layout component

--- pages/leaderboard.js
+++ pages/leaderboard.js
@@ -1,18 +1,17 @@
 import React from 'react';
 
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import Layout from '../components/Layout';
 import Leaderboard from '../components/Leaderboard';
 
 export default function Home() {
     return (
-        <div className='w-5/6 m-auto'>
-            <Header />
-            <div className='section-title'>
-                <h2>Top Journeyers</h2>
-            </div>
-            <Leaderboard />
-            <Footer />
+        <div>
+            <Layout title='Leaderboard'>
+                <div className='section-title'>
+                    <h2>Top Journeyers</h2>
+                </div>
+                <Leaderboard />
+            </Layout>
         </div>
     );
 }

9ffaf2def5a0e1ff947e657a066698f4fdaac12f
Antonio Lo Fiego <alofiego@hey.com>
2020-08-19 14:37:24 -0400

    Refactor homepage to use Layout component

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,16 @@
-import Head from 'next/head';
-
 import React from 'react';
 
-import Header from '../components/Header';
+import Layout from '../components/Layout';
+
 import Tags from '../components/Tags';
 import FeaturedArticle from '../components/FeaturedArticle';
 import NewsCarousel from '../components/NewsCarousel';
-import Footer from '../components/Footer';
 import HomeLeaderboard from '../components/HomeLeaderboard';
 
 export default function Home() {
     return (
-        <div className='w-5/6 m-auto'>
-            <Head>
-                <title>#100DaysOfCloud</title>
-                <link rel='icon' href='/favicon.png' />
-            </Head>
-
-            <Header />
-
-            <main>
+        <div>
+            <Layout>
                 <div className='grid grid-cols-10 gap-8'>
                     <div className='col-span-2'>
                         <Tags />
@@ -34,8 +25,7 @@ export default function Home() {
                         <HomeLeaderboard />
                     </div>
                 </div>
-            </main>
-            <Footer />
+            </Layout>
         </div>
     );
 }

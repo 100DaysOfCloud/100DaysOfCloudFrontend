2f21eb904cb70d2d12e9147dc2dbc5f8d1921cfd
Antonio Lo Fiego <alofiego@hey.com>
2020-08-23 19:16:51 -0400

    Create profile icon components

---
+++ components/subcomponents/ProfileComponents.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import Link from 'next/link';
+
+const NameAndHandle = ({ name, handle, size }) => {
+    return (
+        <div className={`text-${size}`}>
+            <p className='text-gray-900 leading-none cursor-default'>{name}</p>
+            <a
+                href={`https://twitter.com/${handle}`}
+                target='blank'
+                className='text-gray-600 hover:underline'>
+                {`@${handle}`}
+            </a>
+        </div>
+    );
+};
+
+const ProfileImage = ({ id, name, avatar, size }) => {
+    const sizeMappings = {
+        sm: 20,
+        md: 32,
+        lg: 40,
+        xl: 48,
+    };
+
+    return (
+        <div
+            className={`rounded-full overflow-hidden h-${sizeMappings[size]} w-${sizeMappings[size]}`}>
+            <Link href={`/users/${id}`}>
+                <img
+                    src={avatar}
+                    alt={`This is ${name}!`}
+                    className='object-cover object-center cursor-pointer'
+                />
+            </Link>
+        </div>
+    );
+};
+
+const ProfileBio = ({ bio, size }) => {
+    return (
+        <div className=''>
+            <p className={`text-${size} text-gray-700 text-left`}>{bio}</p>
+        </div>
+    );
+};
+
+export { NameAndHandle, ProfileImage, ProfileBio };

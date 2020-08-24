2718eb78b9259e5117baa7192e4526161d651e1e
Antonio Lo Fiego <alofiego@hey.com>
2020-08-23 23:56:34 -0400

    Implemented Auth context at app level

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,48 @@
-import '../styles/styles.css';
+import React from 'react';
 
+import '../styles/styles.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
-function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
-}
+export const AuthContext = React.createContext();
 
-export default MyApp;
+const initialState = {
+    isAuthenticated: false,
+    user: null,
+    token: null,
+};
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'LOGIN':
+            localStorage.setItem('user', JSON.stringify(action.payload.user));
+            localStorage.setItem('token', JSON.stringify(action.payload.token));
+            return {
+                ...state,
+                isAuthenticated: true,
+                user: action.payload.user,
+                token: action.payload.token,
+            };
+        case 'LOGOUT':
+            localStorage.clear();
+            return {
+                ...state,
+                isAuthenticated: false,
+                user: null,
+            };
+        default:
+            return state;
+    }
+};
+
+export default function MyApp({ Component, pageProps }) {
+    const [state, dispatch] = React.useReducer(reducer, initialState);
+    return (
+        <AuthContext.Provider
+            value={{
+                state,
+                dispatch,
+            }}>
+            <Component {...pageProps} />;
+        </AuthContext.Provider>
+    );
+}
